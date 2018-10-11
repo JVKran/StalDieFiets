925b6ffe343c96038dd6024edeaa6b970cbdd013
CvMoerkerk <cas.vanmoerkerk@student.hu.nl>
2018-10-11 13:24:52 +0200

    GUI

--- GUI.py
+++ GUI.py
@@ -1,31 +1,33 @@
 from tkinter import *
 root = Tk()
-root.configure(background='#ffff00')
+root.configure(background='white')
+root.resizable(False, False)
+
 
 label = Label(master=root,
               text='Are you a new user or an existing user?',
-              background='#ffff00',
-              foreground='white',
-              font=('Arial', 27, 'bold'),
-              width=26,
-              height=8,
+              background='#fff517',
+              foreground='black',
+              font=('Arial', 39, 'bold',),
+              width=20,
+              height=5,
               wraplength=500,
               justify='left')
 label.pack()
 
 button = Button(master=root,
-                background='white',
-                font=('Arial', 12, 'bold'),
-                width=20,
-                height=3,
+                background='#c4c4c4',
+                font=('Arial', 30, 'bold'),
+                width=10,
+                height=1,
                 text='New')
 button.pack(pady=15, side=LEFT, padx=35)
 
 button = Button(master=root,
-                background='white',
-                font=('Arial', 12, 'bold'),
-                width=20,
-                height=3,
+                background='#c4c4c4',
+                font=('Arial', 30, 'bold'),
+                width=10,
+                height=1,
                 text='Existing',)
 button.pack(pady=15, anchor='e', padx=35)
 
