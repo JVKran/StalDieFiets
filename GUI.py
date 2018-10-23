f6a8fd130fe2e4acf44d684dff45c135a179e98a
CvMoerkerk <cas.vanmoerkerk@student.hu.nl>
2018-10-23 11:23:24 +0200

    GUI

--- GUI.py
+++ GUI.py
@@ -1,22 +1,26 @@
 from tkinter import *
 import tkinter as tk
+def create_window():
+    login=tk.Toplevel(master=root,
+                       width=500,
+                       height=300,
+                      )
 root = Tk()
 root.resizable(False, False)
 root.configure(background='white')
-root.wm_attributes('-transparentcolor', 'blue')
 
-def create_window():
-    window=tk.Toplevel(root)
-
-photo = PhotoImage(file='grad.gif', width=600, height=300)
+photo = PhotoImage(file='grad.gif', width=500, height=300)
 explanation = "Als je gebruik wilt maken van StalDieFiets heb je een account nodig:"
 
+
 grad = Label(master=root,
-             image=photo)
+             image=photo,
+             padx=0,
+             pady=0)
 
 grad.pack()
 
-exp = Label(master=root,
+exp = Label(master=grad,
              padx=10,
              text=explanation)
 
